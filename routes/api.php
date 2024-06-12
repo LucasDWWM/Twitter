a5e668a8ad370de64e4c9cb93c280b12a69023c2
LucasDWWM <lucaslipari06@gmail.com>
2024-06-12 17:57:34 +0200

    Fix API

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,18 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return response()->json(['message' => 'Welcome to the API'], 200);
+});
+
+
 // Groupement des routes avec middleware d'authentification
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('articles', ArticleController::class);
     Route::apiResource('comments', CommentController::class);
     Route::apiResource('categories', CategoryController::class);
     Route::apiResource('tags', TagController::class);
+    
 });
 
 
